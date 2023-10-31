969d29ad698b54d96a5b115f36a83153ed55a6a6
Jakub <jakubrafalski96@gmail.com>
2023-10-31 18:28:54 +0000

    new-nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,15 @@
 const burgerBtn = document.querySelector('.hamburger');
 const navItems = document.querySelector('.nav-items');
+const navItem = document.querySelector('.nav-item');
 
 const showList = () => {
 	navItems.classList.toggle('active');
 	burgerBtn.classList.toggle('is-active');
 };
 
+const hideList = () => {
+	navItems.classList.remove('active');
+};
+
+navItem.addEventListener('click', hideList);
 burgerBtn.addEventListener('click', showList);
